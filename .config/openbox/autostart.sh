d5b97a54345acecec846b53ab993a70500cbef15
Péter Gombos <gombos@stud.ntnu.no>
2012-05-14 15:42:40 +0200

    added xdefaults and openbox

---
+++ .config/openbox/autostart.sh
@@ -0,0 +1,10 @@
+lxpanel &
+#background-slideshow &
+
+#if [ "$HOSTNAME" = "sprint" ]; then
+#	feh --bg-scale ~/Pictures/emma/emma2.jpg
+#else
+#	feh --bg-max ~/Pictures/emma/emma.jpg ~/Pictures/emma/emma-watson-9
+#fi
+#/home/pvv/d/gombos/.config/openbox/bakgrunn &
+bakgrunn&
